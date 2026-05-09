c0223a6aef09f372f04f22f3d71a05ec8ca5c75e
LeonKing00763 <simbakasele1@gmail.com>
2026-05-09 15:46:40 +0300

    .

---
+++ hellow.py
@@ -0,0 +1,11 @@
+message="hello python world"
+print(message)
+
+message="Hellow python crush course world"
+print(message)
+
+message="Hellow python crush course reader!"
+print(message)
+
+mesage="mambo"
+print(mesage)
